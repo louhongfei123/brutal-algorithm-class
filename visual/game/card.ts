beb3ed158351e9b2baa8dd35cec98d55746aec11
哲の王 <albertcodelab@gmail.com>
2020-06-07 08:40:20 +0000

    game: + todo

--- visual/game/card.ts
+++ visual/game/card.ts
@@ -4,6 +4,7 @@ export class Attack implements Card {
   name = Attack.name;
   constructor() {}
   effect(input: Unit): Unit {
+    // todo: what is the best way to do immutable in JS?
     input.health -= 10;
     return input;
   }
@@ -12,6 +13,7 @@ export class Attack implements Card {
 export class Heal implements Card {
   name = Heal.name;
   constructor() {}
+  // todo: when choose heal, should ask the user to select the target instead of applying to the opponent directly
   effect(input: Unit): Unit {
     input.health += 5;
     return input;
