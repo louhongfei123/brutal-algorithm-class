4293456bd3f89cc2d8af79ba2442971ebb349c23
哲の王 <albertcodelab@gmail.com>
2020-06-10 00:56:29 +0000

    load 1 image

--- game/dist/html.js
+++ game/dist/html.js
@@ -41559,8 +41559,18 @@ System.register("libs/pixi", [], function (exports_1, context_1) {
 });
 System.register("entry/html", ["libs/pixi"], function (exports_2, context_2) {
   "use strict";
-  var pixi_js_1, type;
+  var pixi_js_1, type, app;
   var __moduleName = context_2 && context_2.id;
+  //This `setup` function will run when the image has loaded
+  function setup() {
+    //Create the cat sprite
+    // @ts-ignore
+    let cat = new pixi_js_1.PIXI.Sprite(
+      pixi_js_1.PIXI.loader.resources["../asset/card.png"].texture,
+    );
+    //Add the cat to the stage
+    app.stage.addChild(cat);
+  }
   return {
     setters: [
       function (pixi_js_1_1) {
@@ -41575,6 +41585,23 @@ System.register("entry/html", ["libs/pixi"], function (exports_2, context_2) {
       }
       // @ts-ignore
       pixi_js_1.PIXI.utils.sayHello(type);
+      //Create a Pixi Application
+      // @ts-ignore
+      //Create a Pixi Application
+      app = new pixi_js_1.PIXI.Application({
+        width: 256,
+        height: 256,
+        antialias: true,
+        transparent: false,
+        resolution: 1,
+      });
+      //Add the canvas that Pixi automatically created for you to the HTML document
+      document.body.appendChild(app.view);
+      //load an image and run the `setup` function when it's done
+      // @ts-ignore
+      pixi_js_1.PIXI.loader
+        .add("../asset/card.png")
+        .load(setup);
     },
   };
 });
--- game/src/entry/html.ts
+++ game/src/entry/html.ts
@@ -8,3 +8,33 @@ if (!PIXI.utils.isWebGLSupported()) {
 
 // @ts-ignore
 PIXI.utils.sayHello(type);
+
+//Create a Pixi Application
+// @ts-ignore
+//Create a Pixi Application
+let app = new PIXI.Application({
+  width: 256,
+  height: 256,
+  antialias: true,
+  transparent: false,
+  resolution: 1,
+});
+
+//Add the canvas that Pixi automatically created for you to the HTML document
+document.body.appendChild(app.view);
+
+//load an image and run the `setup` function when it's done
+// @ts-ignore
+PIXI.loader
+  .add("../asset/card.png")
+  .load(setup);
+
+//This `setup` function will run when the image has loaded
+function setup() {
+  //Create the cat sprite
+  // @ts-ignore
+  let cat = new PIXI.Sprite(PIXI.loader.resources["../asset/card.png"].texture);
+
+  //Add the cat to the stage
+  app.stage.addChild(cat);
+}
