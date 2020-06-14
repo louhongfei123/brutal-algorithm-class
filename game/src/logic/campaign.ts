4cd292658df25ecad75746940eda021edf8ffcb0
CreatCodeBuild <CreatCodeBuild@github.com>
2020-06-14 14:29:17 -0700

    + campaign

---
+++ game/src/logic/campaign.ts
@@ -0,0 +1,21 @@
+import { Combat } from './combat';
+
+// A compaign is a sequence of combats.
+export class Campaign {
+
+    currentCombatIndex = 0;
+
+    constructor(
+        public combats: Combat[]
+    ) {
+        if(combats.length === 0) {
+            throw new Error('A campaign must has at least 1 combat');
+        }
+        this.combats = combats;
+    }
+
+    currentCombat(): Combat {
+        return this.combats[this.currentCombatIndex];
+    }
+
+}
